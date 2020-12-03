fb86ba8a4c93da776c104e806d4b9fedf59702b2
AndrewMulkeyCCI <amulkey@candelacontrols.com>
2020-12-03 17:33:24 -0500

    part one working, next integrate the IOUTIL for reading files

--- main.go
+++ main.go
@@ -5,14 +5,26 @@ import "fmt"
 func main() {
 	fmt.Println("Advent of Code Day 1")
 
-	entries := []int{1721, 979, 366, 299, 675, 1456}
-	t := 0
-	for _, s := range entries {
-		t += s + s
-		fmt.Println(s, "+", s, "=", t)
-		if s == 2020 {
-			fmt.Println(t, "+", s, "= 2020")
+	var entries []int = []int{1721, 979, 366, 299, 675, 1456, 4, 4}
+
+	for i, transaction1 := range entries {
+		for j := i + 1; j < len(entries); j++ {
+			transaction2 := entries[j]
+			if transaction1+transaction2 == 2020 {
+				fmt.Println(transaction1, "+", transaction2, "= 2020")
+				fixreport := transaction1 * transaction2
+				fmt.Println("Give the elves this number", fixreport)
+			}
 		}
 	}
+
+	/* This code runs correctly, lets see if we can make it cleaner.
+	for i, transaction1 := range entries {
+		for j, transaction2 := range entries {
+			if transaction1+transaction2 == 2020 && j > i {
+				fmt.Println(transaction1, "+", transaction2, "= 2020")
+			}
+		}
+	}*/
 
 }
